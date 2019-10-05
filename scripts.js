9b5b4f11aadd09f6e4d24b3c88520008bd3b2942
Mark Neumann <mark.neumann@westrock.com>
2019-10-04 22:48:40 -0500

    add form and functions hw 6

--- scripts.js
+++ scripts.js
@@ -1,6 +1,35 @@
-// Enter a description of this program here.
+// simple arithmetic calculator app
 
 /**
  * Write some pseudo code steps here.
  * Use those steps as your `commit` messages (and delete them from here as you complete a step.)
  */
+function add(a,b) {
+  return Number(a) + Number(b);
+}
+
+function subtract(a,b) {
+  return a-b;
+}
+
+function multiply(a,b) {
+  return a*b;
+}
+
+function divide(a,b) {
+  return a / b;
+}
+
+function remainder(a,b) {
+  return a % b;
+}
+
+const form=document.querySelector("form");
+const num1=document.querySelector("#num-1");
+const num1=document.querySelector("#num-2");
+const num1=document.querySelector("select");
+form.addEventListener("submit", function(event){
+  event.preventDefault();
+
+  console.log(num1.value,num2.value,select.value)
+});
