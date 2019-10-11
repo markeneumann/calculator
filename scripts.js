755c09cc1cf8ce704940b7b4d3fc2dacfb64bdbf
Mark Neumann <mark.neumann@westrock.com>
2019-10-11 17:07:43 -0500

    use bracket notation to invoke the appropriate method

--- scripts.js
+++ scripts.js
@@ -26,13 +26,17 @@
   return a % b;
 }
 };
-console.log(calculator.add(1,2));
+
 const form=document.querySelector("form");
 const num1=document.querySelector("#num-1");
-const num1=document.querySelector("#num-2");
-const num1=document.querySelector("select");
+const num2=document.querySelector("#num-2");
+const select=document.querySelector("select");
+const output=document.querySelector("output");
+
 form.addEventListener("submit", function(event){
+
   event.preventDefault();
 
-  console.log(num1.value,num2.value,select.value)
+
+  output.textContent = calculator[select.value]( num1.value, num2.value);
 });
